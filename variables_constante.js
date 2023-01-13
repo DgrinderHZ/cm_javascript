3ebf562f107ef1c3f082dfd16191ae43c78f6cb8
DgrinderHZ <hassanzekkouri@gmail.com>
2023-01-13 17:53:54 +0100

    fin var et const

---
+++ variables_constante.js
@@ -0,0 +1,28 @@
+console.log("Alo Javascript!");
+var nom;
+let prenom;
+
+nom = "Hassan";
+prenom = "Zekkouri";
+console.log("Mon nom est " + nom);
+console.log("Mon prenom est " + prenom);
+
+console.log("Bonjour tout le monde!");
+
+// Declaration des variables
+var a = 2;
+let uneVar = "Hassan";
+let x = 2.6;
+
+console.log("a= " + a);
+console.log("uneVar= " + uneVar);
+console.log("x= " + x);
+
+// Declaration des constante 
+const PI = 3.14;
+
+let perimetre;
+let rayon = 5;
+perimetre = PI * rayon*rayon;
+
+console.log("Le perimetre d'une cercle de rayon = "+ rayon +" est "+ perimetre);
